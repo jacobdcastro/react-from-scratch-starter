5799f8dfa8103bfa1cd843ac0793b7ca05c1ec16
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-12 22:00:24 -0800

    inital config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', 'json'],
+  },
+  externals: ['react-helmet'],
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 2222,
+    publicPath: 'http://localhost:2222/dist/',
+    hot: true,
+  },
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    publicPath: '/build/',
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), new HtmlWebpackPlugin()],
+};
